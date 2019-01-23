21dae326791810815a158ba7a8387157e27adfa2
Nanda Pradipta <37022822+muhnandap@users.noreply.github.com>
2019-01-23 19:55:40 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+consumer_key = 'SVXNR3ImFr8jcH0HTqOsJyxE7'
+consumer_secret = 'JErFvhFYNIrPO2RKtDutgpb1OnC60bVW1FJQpZTOgxdJRWTEWM'
+access_token = '65361192-h0xfsYa61Q11hevsZrb69wQM411XGzSddbtL2Wdh8'
+access_secret = 'EYwWGP2z1d3nQzkvhjr422ulRStmmerQBozWk7AJHllU8'
+
